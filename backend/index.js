71f239ab9c5de5d6d69abddf570b160beaccedaa
schddan <danielschmitzsc@gmail.com>
2024-11-13 12:29:36 -0300

    Novas rotas

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,62 @@ app.post("./register", async (req, res) => {
         res.status(500).send("Erro no servidor")
     }
 })
+
+app.post("./participar_campeonato", async (req, res) => {
+    const {idUsuario} = req.body;
+    try{
+        await pool.query(
+            'UPDATE usuarios SET campeonatos_participados = campeonatos_participados + 1 WHERE id = $1', [idUsuario]
+        )
+        res.status(200).send("Participação registrada")
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send("Erro de servidor")
+    }
+})
+
+app.get('./campeonatos_participados/:id', async (req, res) => {
+    const {idUsuario} = req.params.id
+    
+    try{
+        const resultado = await pool.query("SELECT campeonatos_participados FROM usuarios WHERE id =  $1", {idUsuario})
+
+        if(resultado.rows.length === 0 ){
+            return res.status(404).send("Usuário não encontrado")
+        }
+
+        res.json({totalCampeonatos: resultado.rows[0].campeonatos_participados})
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send("Erro de servidor")
+    }
+})
+
+app.post("/login", async (req, res) => {
+    const {email,senha} = req.body
+    try{
+        const resultado = await pool.query(
+            "SELECT * FROM usuarios WHERE email = $1", {email}
+        )
+        const usuario = resultado.rows[0]
+        if(!usuario){
+            return res.status(400).send("Usuário não encontrado")
+        }
+
+        const verificaSenha = await bcrypt.compare(senha, usuario.senha)
+
+        if(!verificaSenha){
+            return res.status(400).send("Senha incorreta")
+
+        }
+
+        res.send("Login efetuado")
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send("Erro de servidor")
+    }
+})
+
 app.get("/", (req, res) => {
     res.send("Campeonato de Fifa")
 })
